e6ef4a4f29edb987b3cd9dc5f5c4720b867ca0dc
VictoriqueMoe <loli@victorique.moe>
2023-12-30 22:05:40 +0000

    better implementation

--- priorityQueue.ts
+++ priorityQueue.ts
@@ -1,175 +1,50 @@
-// Priority Queue 
-interface IQueue<T> {
-    enqueue(dataItem: T, priority:number): void;
-    dequeue(): T | null;
-    empty: boolean;
-    size: number;
- }
+// Priority Queue
+import {MaxPriorityQueue} from "@datastructures-js/priority-queue";
 
-// Victorias Improved Queue
-class SimpleQueue<T> implements IQueue<T> {
-    private queueData: T[] = [];
-    
-    public get empty(): boolean {
-        return this.queueData.length<=0;
-    }
+const range: number = 10000000;
 
-    public get size(): number {
-        return this.queueData.length;
-    }
+let end: number = 0;
+let count: number = 0;
+let start: number = 0;
+let testOut: string = '';
 
-    public enqueue(dataItem: T, priority:number = 1): void {
-        this.queueData.push(dataItem);
-    }
-
-    public dequeue(): T | null {
-        return this.empty ? null : this.queueData.shift() ?? null;
-    }
+type IPair = {
+    value: number
+    priority: number
 }
 
-// Simple implementation using [T,prio] and sort
-class SimplePriorityQueue<T> implements IQueue<T> {
-    private queueData: [prio:number,value:T][] = [];
-    
-    public get empty(): boolean {
-        return this.queueData.length<=0;
-    }
+const queue = new MaxPriorityQueue<IPair>(value => value.priority);
 
-    public get size(): number {
-        return this.queueData.length;
-    }
-
-    public enqueue(dataItem: T, priority:number = 1): void {
-        this.queueData.push([priority,dataItem]);
-        this.queueData.sort((a,b) => {return b[0]-a[0]});
-    }
-
-    public dequeue(): T | null {
-        if(this.empty) return null;
-        const head = this.queueData.shift() ?? [null,null];
-        return head[1];
-    }
-}
-
-// Map based implementation using map prio:array
-class MapPriorityQueue<T> implements IQueue<T> {
-    private queueData: Map<number,T[]> = new Map<number,T[]>();
-    
-    public get empty(): boolean {
-        return this.queueData.size<=0;
-    }
-
-    public get size(): number {
-        return this.queueData.size;
-    }
-
-    public enqueue(dataItem: T, priority:number = 1): void {
-        let qdata:T[] = this.queueData.get(priority) ?? [];
-        qdata.push(dataItem);
-        this.queueData.set(priority,qdata);
-    }
-
-    public dequeue(): T | null {
-        if(this.empty) return null;
-        let priority:number = Math.max(...this.queueData.keys());
-        let qdata:T[] = this.queueData.get(priority) ?? [];
-        let item:T|null = qdata.shift() ?? null;
-        if(qdata.length === 0) {
-            this.queueData.delete(priority);
-        } else {
-            this.queueData.set(priority,qdata);
-        }
-        return item ?? null;
-    }
-}
-
-const range:number = 10000;
-let start:number = 0, end:number = 0, count:number = 0;
-let testOut:string = '';
-
-// Simple queue
-const simpleQ = new SimpleQueue();
-start = performance.now();
-for(let i=0; i<range; i++) {
-    simpleQ.enqueue(Math.random()*range);
-}
-end = performance.now()
-console.log("Simple queue enqueue time:", end-start);
-
-start = performance.now();
-count = 0;
-while(!simpleQ.empty) {
-    simpleQ.dequeue();
-    count++;
-}
-end = performance.now()
-console.log("Simple queue dequeued items:",count);
-console.log("Simple queue dequeue time:", end-start);
-console.log("-----");
-
-// Simple priority queue
-const simplePrioQ = new SimplePriorityQueue();
-
-console.log("Simple Priority Enqueued 1:1,2:1,3:1,4:5,5:9");
-simplePrioQ.enqueue(1,1);
-simplePrioQ.enqueue(2,1);
-simplePrioQ.enqueue(3,1);
-simplePrioQ.enqueue(4,5);
-simplePrioQ.enqueue(5,9);
-
-while(!simplePrioQ.empty) {
-    testOut += " " + simplePrioQ.dequeue();
-}
-console.log("Simple Priority Queue Dequeued:",testOut); 
-
-start = performance.now();
-for(let i=0; i<range; i++) {
-    simplePrioQ.enqueue(Math.random()*range,Math.floor(Math.random()*9)+1);
-}
-end = performance.now()
-console.log("Simple Priority queue enqueue time:", end-start);
-
-start = performance.now();
-count = 0;
-while(!simplePrioQ.empty) {
-    simplePrioQ.dequeue();
-    count++;
-}
-end = performance.now()
-console.log("Simple priority queue dequeued items:",count);
-console.log("Simple priority queue dequeue time:", end-start);
-console.log("-----");
-
-// Map priority queue
-const mapPrioQ = new MapPriorityQueue();
-
-console.log("Map Priority Enqueued 1:1,2:1,3:1,4:5,5:9");
-mapPrioQ.enqueue(1,1);
-mapPrioQ.enqueue(2,1);
-mapPrioQ.enqueue(3,1);
-mapPrioQ.enqueue(4,5);
-mapPrioQ.enqueue(5,9);
+console.log("Priority Enqueued 1:1,2:1,3:1,4:5,5:9");
+queue.enqueue({value: 1, priority: 1});
+queue.enqueue({value: 2, priority: 1});
+queue.enqueue({value: 3, priority: 1});
+queue.enqueue({value: 4, priority: 5});
+queue.enqueue({value: 5, priority: 9});
 
 testOut = '';
-while(!mapPrioQ.empty) {
-    testOut += " " + mapPrioQ.dequeue();
+while (!queue.isEmpty()) {
+    testOut += " " + queue.dequeue().value;
 }
-console.log("Map Priority Queue Dequeued:",testOut); 
+console.log("Priority Queue Dequeued:", testOut);
 
 start = performance.now();
-for(let i=0; i<range; i++) {
-    mapPrioQ.enqueue(Math.random()*range,Math.floor(Math.random()*9)+1);
+for (let i = 0; i < range; i++) {
+    queue.enqueue({
+        value: Math.random() * range,
+        priority: Math.floor(Math.random() * 9) + 1
+    });
 }
 end = performance.now()
-console.log("Map Priority queue enqueue time:", end-start);
+console.log(`Priority queue enqueue time: ${end - start}ms`);
 
 start = performance.now();
 count = 0;
-while(!mapPrioQ.empty) {
-    mapPrioQ.dequeue();
+while (!queue.isEmpty()) {
+    queue.dequeue();
     count++;
 }
 end = performance.now()
-console.log("Map priority queue dequeued items:",count);
-console.log("Map priority queue dequeue time:", end-start);
+console.log("priority queue dequeued items:", count);
+console.log(`priority queue dequeue time: ${end - start}ms`);
 console.log("-----");
